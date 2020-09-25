ed6aae3534250d2e0199e73293450ef80165fcd3
Bo Peng <ben.bog@gmail.com>
2020-09-25 13:12:50 -0500

    Add option --as-proportion to plugin init

--- covid19_outbreak_simulator/plugins/init.py
+++ covid19_outbreak_simulator/plugins/init.py
@@ -1,5 +1,5 @@
 import random
-
+import numpy as np
 from covid19_outbreak_simulator.event import Event, EventType
 from covid19_outbreak_simulator.plugin import BasePlugin
 from covid19_outbreak_simulator.utils import parse_param_with_multiplier
@@ -28,11 +28,20 @@ class init(BasePlugin):
         parser.add_argument(
             '--seroprevalence',
             nargs='*',
-            help='''Seroprevalence of the population (default to zero). The seroprevalence
-            should always be greater than or euqal to initial incidence rate. The difference
-            between seroprevalence and incidence rate will determine the "recovered" rate of
-            the population. Multipliers are allowed to specify seroprevalence for each
-            group.''')
+            help='''Seroprevalence of the population (default to zero). This aprameter
+            specify the probability (or proportion if --as-proportion is set) of idividuals
+            who have had the virus but have recovered, and will not be infected again (according
+            to current simulation model, which might change later).''')
+        parser.add_argument(
+            '--as-proportion',
+            action='store_true',
+            help='''Seroprevalence and incidence rates are considered as probabilities.
+            However, if you are simulating a large population and would like to ensure the
+            same proportion of infected individuals across replicate simulations, you can
+            set "--as-proportion" to intepret incidence rae and seroprevalence as proportions.
+            Note that noone will be carrying the virus if the population size * proportion is
+            less than 1.'''
+        )
         parser.add_argument(
             '--leadtime',
             help='''With "leadtime" infections are assumed to happen before the simulation.
@@ -54,42 +63,77 @@ class init(BasePlugin):
         isp = parse_param_with_multiplier(args.seroprevalence,
                 subpops=population.group_sizes.keys(), default=0.0)
 
-        events = []
-        for name, sz in population.group_sizes.items():
-            pop_ir = ir.get(name if name in ir else '', 0.0)
-            n_ir = int(sz * pop_ir)
+        infected = []
+        if args.as_proportion:
+            events = []
+            n_ir = 0
+            n_isp = 0
+            for name, sz in population.group_sizes.items():
+                pop_ir = ir.get(name if name in ir else '', 0.0)
+                sp_ir = int(sz * pop_ir)
 
-            pop_isp = isp.get(name if name in isp else '', 0.0)
-            if pop_isp == 0.0:
-                n_recovered = 0
-            elif pop_isp < pop_ir:
-                raise ValueError(
-                    'Seroprevalence, if specified, should be greater than or equal to incidence rate.'
-                )
-            else:
-                n_recovered = int(sz * (pop_isp - pop_ir))
-            pop_status = [1] * n_ir + [2] * n_recovered + [0] * (
-                sz - n_ir - n_recovered)
-            random.shuffle(pop_status)
+                pop_isp = isp.get(name if name in isp else '', 0.0)
+                sp_isp = int(sz * pop_isp)
+                sp_isp = min(sp_isp, sz - sp_ir)
 
-            self.logger.write(
-                f'{self.logger.id}\t{time:.2f}\t{EventType.PLUGIN.name}\t.\tname=init,n_recovered={n_recovered},n_infected={n_ir}\n'
-            )
-            for idx, sts in zip(range(0, sz), pop_status):
-                if sts == 2:
-                    population[f'{name}_{idx}' if name else str(idx)].infected = -10.0
-                    population[f'{name}_{idx}' if name else str(idx)].recovered = -2.0
-                if sts == 1:
-                    events.append(
-                        Event(
-                            0.0,
-                            EventType.INFECTION,
-                            target=name + str(idx),
-                            logger=self.logger,
-                            priority=True,
-                            by=None,
-                            leadtime=args.leadtime,
-                            handle_symptomatic=self.simulator.simu_args
-                            .handle_symptomatic))
+                pop_status = [1] * sp_ir + [2] * sp_isp + [0] * (
+                    sz - sp_ir - sp_isp)
+                random.shuffle(pop_status)
+
+                n_ir += sp_ir
+                n_isp += sp_isp
+                for idx, sts in zip(range(0, sz), pop_status):
+                    if sts == 2:
+                        population[f'{name}_{idx}' if name else str(idx)].infected = -10.0
+                        population[f'{name}_{idx}' if name else str(idx)].recovered = -2.0
+                    if sts == 1:
+                        ID = f'{name}_{idx}' if name else str(idx)
+                        infected.append(ID)
+                        events.append(
+                            Event(
+                                0.0,
+                                EventType.INFECTION,
+                                target=ID,
+                                logger=self.logger,
+                                priority=True,
+                                by=None,
+                                leadtime=args.leadtime,
+                                handle_symptomatic=self.simulator.simu_args
+                                .handle_symptomatic))
+        else:
+            # initialize as probability
+            events = []
+            n_ir = 0
+            n_isp = 0
+            for name, sz in population.group_sizes.items():
+                pop_ir = ir.get(name if name in ir else '', 0.0)
+                pop_isp = isp.get(name if name in isp else '', 0.0)
+                pop_isp = min(pop_isp, 1 - pop_ir)
+
+                pop_rng = np.random.uniform(0, 1, sz)
+                for idx, rng in zip(range(0, sz), pop_rng):
+                    if rng < pop_ir:
+                        n_ir += 1
+                        ID = f'{name}_{idx}' if name else str(idx)
+                        infected.append(ID)
+                        events.append(
+                            Event(
+                                0.0,
+                                EventType.INFECTION,
+                                target=ID,
+                                logger=self.logger,
+                                priority=True,
+                                by=None,
+                                leadtime=args.leadtime,
+                                handle_symptomatic=self.simulator.simu_args
+                                .handle_symptomatic))
+                    elif rng < pop_ir + pop_isp:
+                        n_isp += 1
+                        population[f'{name}_{idx}' if name else str(idx)].infected = -10.0
+                        population[f'{name}_{idx}' if name else str(idx)].recovered = -2.0
+        infected_list = f',infected={",".join(infected)}' if infected else ""
+        self.logger.write(
+           f'{self.logger.id}\t{time:.2f}\t{EventType.PLUGIN.name}\t.\tname=init,n_recovered={n_isp},n_infected={n_ir}{infected_list}\n'
+        )
 
         return events
