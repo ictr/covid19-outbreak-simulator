77cccf5eff9da1034e0da9ab04c58549d945e4cc
Bo Peng <ben.bob@gmail.com>
2021-06-30 10:00:51 -0500

    Report infected individuals in moved

--- covid19_outbreak_simulator/plugins/move.py
+++ covid19_outbreak_simulator/plugins/move.py
@@ -60,20 +60,24 @@ class move(BasePlugin):
         return parser
 
     def move(self, time, population, args, IDs):
+        n_infected = 0
         ID_map = {}
         for ID in IDs:
             new_id = population.move(ID, args.to_subpop)
             if args.reintegrate and isinstance(population[new_id].quarantined, float):
                 population[new_id].reintegrate()
-
-            ID_map[ID] = new_id
+            if isinstance(population[new_id].infected, float) and not isinstance(population[new_id].recovered, float):
+                n_infected += 1
+                ID_map[ID] = new_id + '*'
+            else:
+                ID_map[ID] = new_id
         if args.verbosity > 1 and ID_map:
             ID_map = ',ID_map=' + ','.join(
                 [f'{x}->{y}' for x, y in ID_map.items()])
         else:
             ID_map = ''
         self.logger.write(
-            f'{time:.2f}\t{EventType.PLUGIN.name}\t.\tname=move,from={args.from_subpop},to={args.to_subpop},n_moved={len(IDs)},{ID_map}\n'
+            f'{time:.2f}\t{EventType.PLUGIN.name}\t.\tname=move,from={args.from_subpop},to={args.to_subpop},n_moved={len(IDs)},n_infected={n_infected}{ID_map}\n'
         )
         return []
 
